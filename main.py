8efb92a97a7214cae10bedb2bdcd0a4b1d592f0b
Josh Dudas <josh.dudas@dish.com>
2019-12-17 16:40:39 -0700

    Refactored Converter to single function (need to create slice function).

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from NumberToString import NumToStringConverter
 class getFibNumandConvertToString(unittest.TestCase):
 
     def setUp(self):
-        self.fibNumber = FibValue().getFibNumValue(3)
+        self.fibNumber = FibValue()
 
     def test_convertNumber(self):
-        self.NumConverter = NumToStringConverter(self.fibNumber)
-        self.NumConverter.convert_number_to_string()
+        for i in range(0, 40):
+            self.NumConverter = NumToStringConverter(self.fibNumber.getFibNumValue(i))
+            self.NumConverter.convert_number_to_string()
 
 
 if __name__ == '__main__':

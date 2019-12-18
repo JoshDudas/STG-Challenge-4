cfb6a678409561b03c264aa16357ca796e125962
Josh Dudas <josh.dudas@dish.com>
2019-12-17 17:06:33 -0700

    Added slice function. Confirmed that converter works up to hundred-billions

--- NumberToString.py
+++ NumberToString.py
@@ -2,6 +2,7 @@ class NumToStringConverter:
     def __init__(self, provided_number):
         self.number = str(provided_number)[::-1]
         self.number_length = len(self.number)
+        self.num_slice = ""
         self.number_word_list = []
         self.single_to_teens_digit_words = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
                                             "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen",
@@ -9,41 +10,44 @@ class NumToStringConverter:
         self.tens_digits_words = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 
     def convert_number_to_string(self):
-        if self.number_length % 3 > 0:
-            count = self.number_length // 3 + 1
+        if self.number == '0':
+            print("Zero")
         else:
-            count = self.number_length // 3
-        i = 0
-        start = 0
-        end = 3
-        if self.number != '0':
+            if self.number_length % 3 > 0:
+                count = self.number_length // 3 + 1
+            else:
+                count = self.number_length // 3
+            i = 0
+            start = 0
+            end = 3
             while i < count:
-                num_slice = self.number[start:end]
-                if len(num_slice) == 1:
-                    self.number_word_list.append(self.single_to_teens_digit_words[int(num_slice)])
-                elif len(num_slice) >= 2:
-                    if num_slice[1] == '1':
-                        self.number_word_list.append(self.single_to_teens_digit_words[int("1" + num_slice[0])])
-                        self.number_word_list.append("")
-                    else:
-                        self.number_word_list.append(self.single_to_teens_digit_words[int(num_slice[0])])
-                        self.number_word_list.append(self.tens_digits_words[int(num_slice[1])])
-                if len(num_slice) == 3:
-                    if num_slice[2] != '0':
-                        self.number_word_list.append(self.single_to_teens_digit_words[int(num_slice[2])] + " hundred")
-                    else:
-                        self.number_word_list.append("")
-                if i == 1:
-                    self.number_word_list.insert(3, "thousand")
-                if i == 2:
-                    self.number_word_list.insert(7, "million")
-                if i == 3:
-                    self.number_word_list.insert(9, "billion")
+                self.num_slice = self.number[start:end]
+                self.number_to_word_conversion(i)
                 start = end
                 end += 3
                 i += 1
             self.number_word_list = self.number_word_list[::-1]
             self.number_word_list = ' '.join(self.number_word_list).split()
             print(*self.number_word_list)
-        else:
-            print("Zero")
+
+    def number_to_word_conversion(self, i):
+        if len(self.num_slice) == 1:
+            self.number_word_list.append(self.single_to_teens_digit_words[int(self.num_slice)])
+        elif len(self.num_slice) >= 2:
+            if self.num_slice[1] == '1':
+                self.number_word_list.append(self.single_to_teens_digit_words[int("1" + self.num_slice[0])])
+                self.number_word_list.append("")
+            else:
+                self.number_word_list.append(self.single_to_teens_digit_words[int(self.num_slice[0])])
+                self.number_word_list.append(self.tens_digits_words[int(self.num_slice[1])])
+        if len(self.num_slice) == 3:
+            if self.num_slice[2] != '0':
+                self.number_word_list.append(self.single_to_teens_digit_words[int(self.num_slice[2])] + " hundred")
+            else:
+                self.number_word_list.append("")
+        if i == 1:
+            self.number_word_list.insert(3, "thousand")
+        if i == 2:
+            self.number_word_list.insert(7, "million")
+        if i == 3:
+            self.number_word_list.insert(11, "billion")
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class getFibNumandConvertToString(unittest.TestCase):
         self.fibNumber = FibValue()
 
     def test_convertNumber(self):
-        for i in range(0, 40):
+        for i in range(0, 55):
             self.NumConverter = NumToStringConverter(self.fibNumber.getFibNumValue(i))
             self.NumConverter.convert_number_to_string()
 
